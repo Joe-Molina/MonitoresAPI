e944caa65c651e3800d8ee3bfe6b7935588edd14
jodomodev <joedodaniljr123ggmail.com>
2024-07-08 16:31:10 -0400

    actualizacion: se agrego PUT a posts para actualizar informacion

--- src/routes/publicidades.routes.ts
+++ src/routes/publicidades.routes.ts
@@ -4,6 +4,9 @@ import {
   DeletePublicidad,
   getPublicidades,
 } from "../services/publicidades";
+import { asignarPositions } from "../services/asignarPosistion";
+import { updateEndDate, updateStartDate } from "../services/updateDate";
+import { updateDuration } from "../services/updateDuration";
 
 const router = express.Router();
 
@@ -19,11 +22,40 @@ router.get("/", async (_req, res, next: NextFunction) => {
 router.post("/", async (req, res, next: NextFunction) => {
   try {
     const newPublicidad = await createPublicidad(req.body);
-    res.send(await newPublicidad);
+    res.send(newPublicidad);
   } catch (error) {
     next(error);
   }
 });
+
+router.put("/", async (req, res) => {
+
+  const { fecha_inicio, Fecha_Fin, position, duration, id } =
+    await req.body.edit;
+
+  console.log(req.body)
+
+  console.log({ id, position })
+
+  const newPositions = await asignarPositions(position, req.body.id);
+  const newDuration = await updateDuration(duration, req.body.id);
+  const newEndDate = await updateEndDate(Fecha_Fin, req.body.id);
+  const newStartDate = await updateStartDate(fecha_inicio, req.body.id);
+
+  const data = {
+    newDuration,
+    newEndDate,
+    newPositions,
+    newStartDate,
+  }
+
+  console.log(data)
+
+  return res.json(data);
+
+});
+
+
 
 router.delete("/", async (req, res, next: NextFunction) => {
   try {
